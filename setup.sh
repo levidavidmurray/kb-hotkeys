649abcb40c59217f8b9395dec25cf30ceb3f4249
Levi Murray <levidavidmurray@gmail.com>
2019-01-25 15:06:30 -0800

    moved new_keys cfg file, fixed setup.sh cfg file location

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-cp cfg/.Xmodmap ~/
+mkdir ~/.config/kb-hotkeys
+cp cfg/new_keys ~/.config/kb-hotkeys/
 cp cfg/xmodmap.desktop ~/.config/autostart/
-xmodmap ~/.Xmodmap
+xmodmap ~/.config/kb-hotkeys/new_keys
